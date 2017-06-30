e457bf8671f06c9a94fb545a132afc9268868c8a
Jay Deverett <jayjdeverett@gmail.com>
2017-06-30 13:50:21 -0400

    complete exercise 1

--- exercise.rb
+++ exercise.rb
@@ -1,13 +1,23 @@
 class BankAccount
 
-    def initialize(balance, interest_rate = 0)
+    def initialize(balance, interest_rate = 1.07)
       @balance = balance
       @interest_rate = interest_rate
     end
 
     def deposit(amount)
       new_amount = amount + @balance
-      puts "Your balance after depositing $#{amount} is $#{new_amount}"
+      puts "Your new balance after depositing $#{amount} is $#{new_amount}."
+    end
+
+    def withdraw(amount)
+      new_amount = @balance - amount
+      puts "Your new balance after withdrawing $#{amount} is $#{new_amount}."
+    end
+
+    def gain_interest()
+      amount_with_interest = @balance * @interest_rate
+      puts "Your new balance after interest is $#{amount_with_interest}."
     end
 
 end
@@ -16,3 +26,7 @@ end
 my_account = BankAccount.new(1000)
 
 my_account.deposit(150)
+
+my_account.withdraw(150)
+
+my_account.gain_interest
