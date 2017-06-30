0f096dfa7891b8422d6bc15af8765b1007aefdd0
Jay Deverett <jayjdeverett@gmail.com>
2017-06-30 17:02:32 -0400

    complete paperboy

--- paperboy.rb
+++ paperboy.rb
@@ -10,7 +10,7 @@ class Paperboy
   end
 
   def quota()
-    return 50 + @experience/2
+    quota = 50 + @experience/2
   end
 
   def deliver(start_address, end_address)
@@ -20,12 +20,14 @@ class Paperboy
     @experience =  @experience + new_experience
 
 
-    total_houses * 0.25 = new_earnings
+    new_earnings = total_houses * 0.25
     @earnings = @earnings + new_earnings
-    if new_earnings < @quota
+
+    if new_experience < @quota
       @earnings = @earnings - 2.0
     elsif new_earnings >= @quota
-      
+      over_quota = new_experience - @quota
+      @earnings = @earnings + (over_quota * 0.25)
     end
 
 
@@ -40,7 +42,7 @@ class Paperboy
 
 end
 
-paperboy1 = Paperboy.new("Billy", @experience, @earnings)
+paperboy1 = Paperboy.new("Billy", 0, 0)
 
-paperboy1.deliver(1, 10)
+paperboy1.deliver(50, 75)
 paperboy1.report
