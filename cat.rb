ac1fafd4e1b956808147c30aee7cd86f0dc413bf
Jay Deverett <jayjdeverett@gmail.com>
2017-06-30 14:21:32 -0400

    add completed cats exercise

---
+++ cat.rb
@@ -0,0 +1,33 @@
+class Cat
+
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+  end
+
+
+  def eats_at()
+    if @meal_time < 6
+      puts "#{@meal_time} AM"
+    else
+      puts "#{@meal_time} PM"
+    end
+  end
+
+  def meow()
+    puts "Hi! I'm #{@name}. I eat #{@preferred_food} and I like to eat
+    at #{@meal_time} sharp!"
+  end
+
+
+end
+
+cat1 = Cat.new("Sparkles", "tuna", 2)
+cat2 = Cat.new("Arnold", "salmon", 22)
+
+# cat2.eats_at
+# cat1.eats_at
+
+cat1.meow
+cat2.meow
